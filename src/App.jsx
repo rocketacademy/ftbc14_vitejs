19a4ec6961516e493f171f23298783e7d07f7cdf
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2023-11-21 10:11:01 +0800

    day 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import logo from "/rocket.png";
+import "./App.css";
+import Footer, { additonalJSX, test } from "./Components/Footer.jsx";
+import Button from "react-bootstrap/Button";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  console.log("Good morning indra");
+  const classSize = 9;
+  const classFTBC14 = <h1>Hello world welcome {classSize} students</h1>;
+  const classList = (
+    <ol>
+      <li>Indra</li>
+      <li>Ben</li>
+      <li>Jasper</li>
+      <li>Iffah</li>
+      <li>Ester</li>
+      <li>Hongyu</li>
+    </ol>
+  );
+  const styleVariables = {
+    color: "#000",
+    border: "1px solid #fff",
+  };
+  const page = (
+    <div style={styleVariables}>
+      {classFTBC14} {classList}
+    </div>
+  );
 
   return (
     <>
+      {page}
+
+      {2 + 2}
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={logo} className="logo react" alt="Rocket logo" />
       </div>
-      <h1>Vite + React</h1>
+      <h1 style={{ backgroundColor: "#fff", color: "#000" }}>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <Button variant="outline-danger">Click me</Button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -28,8 +51,13 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      {additonalJSX}
+      {test}
+
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <div>
+      <h4>By: Sam </h4>
+    </div>
+  );
+}
+
+export const additonalJSX = (
+  <div>
+    <p>Well done</p>
+  </div>
+);
+
+export const test = <marquee>Woww</marquee>;
